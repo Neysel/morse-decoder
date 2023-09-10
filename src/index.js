ad64152bc16d5f0b972f49ff24b498632db90c7a
Leyla <neyseltheblack@gmail.com>
2023-09-10 13:06:49 +0500

    finally

--- src/index.js
+++ src/index.js
@@ -39,6 +39,41 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    console.log(expr)
+    let morse = ''
+    let decode = ''
+    for (let i = 0; i<expr.length; i+=10) {
+        let number = ''
+            for (let j=0; j<10; j++){
+                
+                number += expr[i +j]
+            }
+
+    for (let i=0; i<number.length; i+=2) {
+        if(number[i]+number[i+1] === '00') {
+
+        } else if (number[i]+number[i+1]  === '10') {
+            morse += '.'
+        } else if (number[i]+number[i+1]  === '11') {
+            morse += '-'
+        } else if (number[i]+number[i+1]  === '**'){
+            morse += '**'
+        }
+    }
+
+    for (let morsekey in MORSE_TABLE) {
+        if (morsekey === morse) {
+            decode += MORSE_TABLE[morsekey]
+            morse = ''
+        } else if (morse === '**********'){
+            decode += ' '
+            morse = ''
+        }
+    }
+    }
+    console.log(decode)
+   return decode
+
 }
 
 module.exports = {
